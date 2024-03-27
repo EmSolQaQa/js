73cecd578a62c2a9c4dc2e91fe5d73263f60bb6b
lemanshamilova <lemansamilova2003@gmail.com>
2024-03-27 08:57:57 -0700

    javascript.intro

---
+++ script.js
@@ -0,0 +1,58 @@
+// task1
+// let a= window.prompt("deyiseni daxil edin");
+// let min = a / 60;
+// let sec = a % 60;
+// console.log("deqiqe:",min,"saniye:",sec)
+// task2
+// let a = window.prompt("temperaturu daxil edin");
+// let celcius = 5 / 9 * a;
+// console.log(celcius)
+// task3
+// let a = +window.prompt("qiymetlerinizi daxil edin");
+// let b = +window.prompt("qiymetlerinizi daxil edin");
+// let c = +window.prompt("qiymetlerinizi daxil edin");
+// let d = +window.prompt("qiymetlerinizi daxil edin");
+// let e = +window.prompt("qiymetlerinizi daxil edin");
+// let gpa = a+b+c+d+e / 5;
+// console.log(gpa)
+// task4
+// let a = +window.prompt("tevelludu daxil edin");
+// let tevellud = 2024 - a;
+// console.log(tevellud)
+// task5
+// let a = +window.prompt("ededi daxil edin");
+// let b = +window.prompt("faizi daxil edin");
+// let faiz = a /100 * b;
+// console.log(faiz)
+// task6
+// let a = +window.prompt("kateti daxil edin");
+// let b = +window.prompt("kateti daxil edin");
+// let hipetonuz = Math.sqrt(a**2+b**2);
+// console.log(hipetonuz)
+// task7
+// let a = +window.prompt("kredit miqdarini daxil edin");
+// let b = +window.prompt("faiz derecesi daxil edin");
+// let c = +window.prompt("kredit muddeti daxil edin");
+// let odenilen_mebleg = (a + (a / 100 *b))/c;
+// console.log(odenilen_mebleg)
+// task8
+// let a = window.prompt("adinizi daxil edin");
+// let b = window.prompt("soyadi daxil edin");
+// console.log("Salam",a,b)
+// task9
+// let a = +window.prompt("kvadratin terefini daxil edin");
+// let perimetr = 4 * a;
+// let sahe = a**2;
+// console.log("Sahesi:",sahe,"Perimetri:",perimetr)
+// task10
+// let a = +window.prompt("birinci ededi daxil edin");
+// let b = +window.prompt("ikinci ededi daxil edin");
+// let cemin_kubu = (a+b)**3;
+// let kubun_cemi = a**3 + b**3;
+// console.log("Cemin kubu:",cemin_kubu,"kubun cemi:",kubun_cemi)
+
+
+
+
+
+
